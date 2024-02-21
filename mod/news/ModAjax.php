7da0aa451d70fdf402b4eefe947732a8489ee421
Alexey <reno.greenleaf@gmail.com>
2024-02-21 11:09:34 +0300

    Setting author.

--- mod/news/ModAjax.php
+++ mod/news/ModAjax.php
@@ -10,6 +10,9 @@ class ModAjax extends ajaxFunc {
      * @return xajaxResponse - ответ будет обработан js функцией ядра
      */
     public function axSaveNewsArticle(array $data): xajaxResponse {
+        $currentUserID = Zend_Registry::getInstance()->get('auth')->ID;
+        $data['control']['author_id'] = $currentUserID;
+
         if ($this->ajaxValidate($data, ['title' => 'req', 'content' => 'req'])) {
             return $this->response;
         }
